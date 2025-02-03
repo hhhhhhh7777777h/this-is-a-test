ff64108f3b2154adfa43bca76421ca45e53d9409
hhhhhhh7777777h <holmberg.hayden1@gmail.com>
2025-02-03 15:48:07 -0600

    Update simple.c

--- simple.c
+++ simple.c
@@ -12,27 +12,40 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/hash.h>
+#include <linux/gcd.h>
+#include <linux/jiffies.h>
+#include <asm/param.h>
 
 /* This function is called when the module is loaded. */
 static int simple_init(void)
 {
-       printk(KERN_INFO "Loading Module\n");
-       printk(KERN_INFO "%lu\n", GOLDEN_RATIO_PRIME);
-       return 0;
+    printk(KERN_INFO "Loading Module\n");
+
+    printk(KERN_INFO "golden: %lu\n", GOLDEN_RATIO_PRIME);
+
+    printk(KERN_INFO "hz: %d\n", HZ);
+    printk(KERN_INFO "jiffies: %lu\n", jiffies);
+
+    return 0;
 }
 
 /* This function is called when the module is removed. */
-static void simple_exit(void) {
-	printk(KERN_INFO "Removing Module\n");
-       printk(KERN_INFO "%lu\n", 12);
+static void simple_exit(void)
+{
+    printk(KERN_INFO "Removing Module\n");
+
+    printk(KERN_INFO "gcd: %lu\n", gcd(3300, 24));
+
+    // Print jiffies at exit
+    printk(KERN_INFO "jiffies: %lu\n", jiffies);
 
 }
 
 /* Macros for registering module entry and exit points. */
-module_init( simple_init );
-module_exit( simple_exit );
+module_init(simple_init);
+module_exit(simple_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Simple Module");
+MODULE_DESCRIPTION("Simple Kernel Module");
 MODULE_AUTHOR("SGG");
-
